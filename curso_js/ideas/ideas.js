6436955837d174f73eaa034fa3fc6878f29441bc
francodangelo <franco@francodangelo.com>
2021-03-02 00:31:27 -0300

    Ideas

--- curso_js/ideas/ideas.js
+++ curso_js/ideas/ideas.js
@@ -1,85 +1,47 @@
-//  Crear un algoritmo simple 
+//  Crear un algoritmo con objetos 
 /*  Crear un script en JS que le solicite al usuario ingresar datos. 
     Luego realizar operaciones con estos datos */
 
 // Crear una calculadora de sueldo bruto a sueldo neto 
 // siguiendo esta pagina: https://www.toptrabajos.com/blog/ar/calcular-sueldo-neto-argentina/
-/*
-console.log("Inicio de programa: Calculadora de Bruto - Neto")
 
-// Define function
+// Definicion funcion de bruto a neto
 function brutoNeto(sueldoBruto) {
     obraSocial = sueldoBruto * 0.03 
     descINSSJP = sueldoBruto * 0.03 
     jubilacion = sueldoBruto * 0.11
-    cuotaSindical = sueldoBruto * porcentajeSindical
 
     // Calculate sueldo neto
-    if (cuotaSindical > 0) {
-        sueldoNeto = sueldoBruto - cuotaSindical - obraSocial - descINSSJP - jubilacion
-    } else {
-        sueldoNeto = sueldoBruto - obraSocial - descINSSJP - jubilacion
-    }
+    sueldoNeto = sueldoBruto - obraSocial - descINSSJP - jubilacion
 
     // Output
     return sueldoNeto
 }
 
-
-// Ask user for inputs
-sueldoBruto = parseInt(prompt("Ingrese su sueldo bruto: "))
-porcentajeSindical = parseFloat(prompt("Porcentaje de Cuota Sindical (si aplica): ") / 100)
-
-// Output
-console.log("Su sueldo Neto es de: " + sueldoNeto + " pesos.")
-
-// Alerts
-alert("Su sueldo Neto es de: " + sueldoNeto + " pesos.")
-alert("Fin de programa")
-
-// Definición de la función
-function sumaYMuestra(primerNumero, segundoNumero) { 
-    var resultado = primerNumero + segundoNumero
-    alert("El resultado es " + resultado)
-  }
-  
-  // Declaración de las variables
-  var numero1 = 3;
-  var numero2 = 5;
-  
-  // Llamada a la función
-  sumaYMuestra(numero1, numero2)
-
-*/
-
-// // Get element by ID
-// var submit = document.getElementById("submit")
-
-// submit.onclick = function() {
-//     var sueldoBruto = document.querySelector("sueldoBruto").value //document.getElementById("sueldoBruto").value
-    
-//     document.getElementById("outputSN").innerText = sueldoBruto
-// }
-  
-
-// Array.from(document.querySelectorAll("#sueldoForm input")).reduce((acc, input) => ({ ...acc, [input.id]: [input.value]}), {})
-
-const   wrapper = document.querySelector('wrap-contact100'),
-        form    = wrapper.querySelectorAll('contact100-form'),
-        submitInput  = form[0].querySelector('input[type="submit"]');
-        // submit = document.getElementById("submitInput")
-
-function    getDataForm(e){
-    // Prevent Default Behaviour of the Submit
-    e.preventDefault();
-
-    var formData = new FormData(form[0]);
-
-    alert( formData.get('sueldoBruto'));
+// Definicion Objeto Persona
+function Persona(bruto, neto) {
+    this.bruto = bruto
+    this.neto = neto
 }
 
-document.addEventListener('DOMContentLoaded', function(){ 
-            
-    submit.addEventListener('click', getDataForm, false);
+// Main Function
+function main() {
+    // Get user inputs
+    var bruto = document.getElementById("input_bruto").value
+    var neto = brutoNeto(bruto)
+    var newPersona = new Persona(bruto, neto)
 
-}, false);
+    document.getElementById("output_neto").innerHTML = neto
+
+    alert(bruto + " " + neto)
+    alert(newPersona.bruto + " " + newPersona.neto)
+}
+
+// Bring button to life
+document.getElementById("btn").addEventListener("click", main);
+// Trigger function using enter 
+function triggerEnter(event) {
+    if (event.which == 13) {
+        main()
+    }
+}
